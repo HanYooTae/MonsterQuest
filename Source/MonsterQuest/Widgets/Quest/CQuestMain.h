3e406552c48668ad1c64176e58b52bf1a940458f
HanYooTae <han931120@gmail.com>
2024-04-01 23:05:42 +0900

    Make CQuestMain

---
+++ Source/MonsterQuest/Widgets/Quest/CQuestMain.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CQuestMain.generated.h"
+
+UCLASS()
+class MONSTERQUEST_API UCQuestMain : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
