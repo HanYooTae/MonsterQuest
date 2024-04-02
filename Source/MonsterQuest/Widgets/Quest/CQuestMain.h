77e496350e61fc69fa87c4697bb628d16b52b7b4
HanYooTae <han931120@gmail.com>
2024-04-02 21:59:57 +0900

    Make Quest Property

--- Source/MonsterQuest/Widgets/Quest/CQuestMain.h
+++ Source/MonsterQuest/Widgets/Quest/CQuestMain.h
@@ -8,5 +8,16 @@ UCLASS()
 class MONSTERQUEST_API UCQuestMain : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+	UPROPERTY(meta = (BindWidget))
+		class UScrollbox* QuestList;
+
+	UPROPERTY(meta = (BindWidget))
+		class UGridPanel* GiftList;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* OkButton;
+
+	UPROPERTY(meta = (BindWidget))
+		class UButton* CancelButton;
 };
