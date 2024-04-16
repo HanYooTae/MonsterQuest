42f5620445298f3e557f3640800735716c31756e
HanYooTae <han931120@gmail.com>
2024-04-16 22:20:16 +0900

    Initialize StateComponent

--- Source/MonsterQuest/ActorComponents/CStateComponent.h
+++ Source/MonsterQuest/ActorComponents/CStateComponent.h
@@ -4,6 +4,13 @@
 #include "Components/ActorComponent.h"
 #include "CStateComponent.generated.h"
 
+UENUM(BlueprintType)
+enum class EStateType : uint8
+{
+	Idle, Equip, Action, Hitted, Dead, Max
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FStateTypeChangedSignature, EStateType, InPrevType, EStateType, InNewType);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MONSTERQUEST_API UCStateComponent : public UActorComponent
@@ -13,8 +20,35 @@ class MONSTERQUEST_API UCStateComponent : public UActorComponent
 public:	
 	UCStateComponent();
 
+public:		// Get StateType
+	bool IsIdleMode() { return Type == EStateType::Idle; }
+	bool IsEquipMode() { return Type == EStateType::Equip; }
+	bool IsActionMode() { return Type == EStateType::Action; }
+	bool IsHittedMode() { return Type == EStateType::Hitted; }
+	bool IsDeadMode() { return Type == EStateType::Dead; }
+
+public:		// Set StateType
+	UFUNCTION()
+		void SetIdleMode();
+	UFUNCTION()
+		void SetEquipMode();
+	UFUNCTION()
+		void SetActionMode();
+	UFUNCTION()
+		void SetHittedMode();
+	UFUNCTION()
+		void SetDeadMode();
+
+	UFUNCTION()
+		void ChangeType(EStateType InNewType);
+
 protected:
 	virtual void BeginPlay() override;
 
-		
+public:
+	UPROPERTY(BlueprintAssignable)
+		FStateTypeChangedSignature OnStateTypeChanged;
+
+private:
+	EStateType Type;
 };
