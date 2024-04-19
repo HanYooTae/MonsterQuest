2eeac00b8b9caf0b95d07662f7eaeb3dccfa73b5
HanYooTae <han931120@gmail.com>
2024-04-19 21:09:18 +0900

    Modify DoActionData

--- Source/MonsterQuest/Actions/DoActions/CDoAction.cpp
+++ Source/MonsterQuest/Actions/DoActions/CDoAction.cpp
@@ -1,11 +1,29 @@
 #include "Actions/DoActions/CDoAction.h"
 
+#include "ActorComponents/CStateComponent.h"
+#include "ActorComponents/CStatusComponent.h"
+
+#include "GameFramework/Character.h"
+
+#include "Global.h"
+
 ACDoAction::ACDoAction()
 {
+	PrimaryActorTick.bCanEverTick = true;
 }
 
 void ACDoAction::BeginPlay()
 {
+	OwnerCharacter = Cast<ACharacter>(GetOwner());
+
+	StatusComp = Cast<UCStatusComponent>(OwnerCharacter->GetComponentByClass(UCStatusComponent::StaticClass()));
+	StateComp = Cast<UCStateComponent>(OwnerCharacter->GetComponentByClass(UCStateComponent::StaticClass()));
+
 	Super::BeginPlay();
-	
+}
+
+void ACDoAction::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
 }
