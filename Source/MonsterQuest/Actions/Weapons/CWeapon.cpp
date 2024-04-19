d1b0c0ed33a1f2a88a769659ee06b7104ff1d975
HanYooTae <han931120@gmail.com>
2024-04-19 21:09:37 +0900

    Modify WeaponData

--- Source/MonsterQuest/Actions/Weapons/CWeapon.cpp
+++ Source/MonsterQuest/Actions/Weapons/CWeapon.cpp
@@ -1,5 +1,10 @@
 #include "Actions/Weapons/CWeapon.h"
 
+#include "GameFramework/Character.h"
+#include "Components/Shapecomponent.h"
+
+#include "Global.h"
+
 ACWeapon::ACWeapon()
 {
 
@@ -7,6 +12,58 @@ ACWeapon::ACWeapon()
 
 void ACWeapon::BeginPlay()
 {
+	OwnerCharacter = Cast<ACharacter>(GetOwner());
+
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &ACWeapon::BeginOverlap);
+	Collision->OnComponentEndOverlap.AddDynamic(this, &ACWeapon::EndOverlap);
+
+	OffCollision();
+
 	Super::BeginPlay();
-	
+}
+
+void ACWeapon::Attachment(FName InSocketName)
+{
+	AttachToComponent
+	(
+		OwnerCharacter->GetMesh(),
+		FAttachmentTransformRules(EAttachmentRule::KeepRelative, true),
+		InSocketName
+	);
+}
+
+void ACWeapon::OnCollision()
+{
+	Collision->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
+
+void ACWeapon::OffCollision()
+{
+	Collision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void ACWeapon::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	CheckTrue(OwnerCharacter == OtherActor);
+
+	ACharacter* otherCharacter = Cast<ACharacter>(OtherActor);
+
+	if (OnBeginOverlap.IsBound())
+	{
+		if (!!otherCharacter)
+			OnBeginOverlap.Broadcast(OwnerCharacter, this, otherCharacter);
+	}
+}
+
+void ACWeapon::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	CheckTrue(OwnerCharacter == OtherActor);
+
+	ACharacter* otherCharacter = Cast<ACharacter>(OtherActor);
+
+	if (OnEndOverlap.IsBound())
+	{
+		if (!!otherCharacter)
+			OnEndOverlap.Broadcast(OwnerCharacter, this, otherCharacter);
+	}
 }
--- Source/MonsterQuest/Actions/Weapons/CWeapon.h
+++ Source/MonsterQuest/Actions/Weapons/CWeapon.h
@@ -4,6 +4,9 @@
 #include "GameFramework/Actor.h"
 #include "CWeapon.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FBeginOverlapSignature, class ACharacter*, InAttacker, class AActor*, InCauser, class ACharacter*, InOtherCharacter);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FEndOverlapSignature, class ACharacter*, InAttacker, class AActor*, InCauser, class ACharacter*, InOtherCharacter);
+
 UCLASS()
 class MONSTERQUEST_API ACWeapon : public AActor
 {
@@ -15,4 +18,39 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+public:
+	UFUNCTION(BlueprintCallable)
+		virtual void Attachment(FName InSocketName);
+
+public:		// 무기 장착 or 해제 시 socket변경
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnEquip();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		void OnUnequip();
+
+public:
+	void OnCollision();
+	void OffCollision();
+
+	UFUNCTION()
+		void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+		void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:
+	UPROPERTY(VisibleAnywhere)
+		class USkeletalMeshComponent* Weapon;
+
+public:
+	UPROPERTY(BlueprintReadOnly)
+		class ACharacter* OwnerCharacter;
+
+public:
+	FBeginOverlapSignature OnBeginOverlap;
+	FEndOverlapSignature OnEndOverlap;
+
+private:
+	class UShapeComponent* Collision;
 };
