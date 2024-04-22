94720d0116e940a4b6f5a579267f146efea141ef
HanYooTae <han931120@gmail.com>
2024-04-22 19:32:10 +0900

    Add ActionComponent

--- Source/MonsterQuest/ActorComponents/CActionComponent.cpp
+++ Source/MonsterQuest/ActorComponents/CActionComponent.cpp
@@ -1,5 +1,13 @@
 #include "ActorComponents/CActionComponent.h"
 
+#include "ActionDatas/CActionData_Spawned.h"
+#include "ActionDatas/CActionData.h"
+#include "Actions/Equipment/CEquipment.h"
+#include "Actions/DoActions/CDoAction.h"
+#include "Actions/Weapons/CWeapon.h"
+
+#include "GameFramework/Character.h"
+
 #include "Global.h"
 
 UCActionComponent::UCActionComponent()
@@ -10,5 +18,120 @@ void UCActionComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	
+	ACharacter* ownerCharacter = Cast<ACharacter>(GetOwner());
+
+	for (int32 i = 0; i < (int32)EActionType::Max; i++)
+	{
+		if (!!DataAssets[i])
+			DataAssets[i]->BeginPlay(ownerCharacter, &Datas[i]);
+	}
+}
+
+void UCActionComponent::SetUnarmedMode()
+{
+	// 무기 해제
+	if (!!Datas[(int32)Type] && !!Datas[(int32)Type]->GetEquipment())
+		Datas[(int32)Type]->GetEquipment()->Unequip();
+
+	ChangeType(EActionType::Unarmed);
+}
+
+void UCActionComponent::SetSwordMode()
+{
+	SetMode(EActionType::Sword);
+}
+
+void UCActionComponent::SetPistolMode()
+{
+	SetMode(EActionType::Pistol);
+}
+
+void UCActionComponent::SetRifleMode()
+{
+	SetMode(EActionType::Rifle);
+}
+
+void UCActionComponent::SetSniperMode()
+{
+	SetMode(EActionType::Sniper);
+}
+
+void UCActionComponent::SetMode(EActionType InNewType)
+{
+	// 같은 키를 2번 눌렀을 때
+	if (Type == InNewType)
+	{
+		SetUnarmedMode();
+
+		return;
+	}
+
+	// Unarmed가 아닌 Type을 장착하고 있었다면
+	else if (IsUnarmedMode() == false)
+	{
+		// 이전 무기 장착 해제
+		if (!!Datas[(int32)Type] && !!Datas[(int32)Type]->GetEquipment())
+			Datas[(int32)Type]->GetEquipment()->Unequip();
+	}
+	// 새로운 무기 장착
+	if (!!Datas[(int32)InNewType] && !!Datas[(int32)InNewType]->GetEquipment())
+		Datas[(int32)InNewType]->GetEquipment()->Equip();
+
+	ChangeType(InNewType);
+}
+
+void UCActionComponent::ChangeType(EActionType InNewType)
+{
+	EActionType prev = Type;
+	Type = InNewType;
+
+	if (OnActionTypeChanged.IsBound())
+		OnActionTypeChanged.Broadcast(prev, InNewType);
+}
+
+void UCActionComponent::DoAction()
+{
+	CheckTrue(IsUnarmedMode());
+
+	if (!!GetCurrentData())
+	{
+		ACDoAction* doAction = GetCurrentData()->GetDoAction();
+		if (!!doAction)
+		{
+			doAction->DoAction();
+		}
+	}
+}
+
+void UCActionComponent::DoAim(bool InPressed)
+{
+}
+
+void UCActionComponent::OffAllCollisions()
+{
+	for (const auto& data : Datas)
+	{
+		if (data == nullptr)
+			continue;
+
+		if (data->GetWeapon() == nullptr)
+			continue;
+
+		data->GetWeapon()->OffCollision();
+	}
+}
+
+void UCActionComponent::End_Dead()
+{
+	for (int32 i = 0; i < (int32)EActionType::Max; i++)
+	{
+		if (!!Datas[i] && !!Datas[i]->GetEquipment())
+			Datas[i]->GetEquipment()->Destroy();
+
+		if (!!Datas[i] && !!Datas[i]->GetWeapon())
+			Datas[i]->GetWeapon()->Destroy();
+
+		if (!!Datas[i] && !!Datas[i]->GetDoAction())
+			Datas[i]->GetDoAction()->Destroy();
+	}
 }
--- Source/MonsterQuest/ActorComponents/CActionComponent.h
+++ Source/MonsterQuest/ActorComponents/CActionComponent.h
@@ -10,6 +10,8 @@ enum class EActionType : uint8
 	Unarmed, Sword, Pistol, Rifle, Sniper, Max
 };
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FActionTypeChangedSignature, EActionType, InPrevType, EActionType, InNewType);
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class MONSTERQUEST_API UCActionComponent : public UActorComponent
 {
@@ -21,5 +23,75 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-		
+public:		// Get Action
+	UFUNCTION(BlueprintPure)
+		class UCActionData_Spawned* GetCurrentData() { return Datas[(int32)Type]; }
+
+	UFUNCTION(BlueprintPure)
+		class UCActionData* GetCurrentDataAsset() { return DataAssets[(int32)Type]; }
+
+public:		// bool Type
+	UFUNCTION(BlueprintPure)
+		FORCEINLINE bool IsUnarmedMode() { return Type == EActionType::Unarmed; }
+
+	UFUNCTION(BlueprintPure)
+		FORCEINLINE bool IsSwordMode() { return Type == EActionType::Sword; }
+
+	UFUNCTION(BlueprintPure)
+		FORCEINLINE bool IsPistolMode() { return Type == EActionType::Pistol; }
+
+	UFUNCTION(BlueprintPure)
+		FORCEINLINE bool IsRifleMode() { return Type == EActionType::Rifle; }
+
+	UFUNCTION(BlueprintPure)
+		FORCEINLINE bool IsSniperMode() { return Type == EActionType::Sniper; }
+
+public:		// Set Action NetMulticast
+	UFUNCTION(BlueprintCallable)
+		void SetUnarmedMode();
+
+	UFUNCTION(BlueprintCallable)
+		void SetSwordMode();
+
+	UFUNCTION(BlueprintCallable)
+		void SetPistolMode();
+
+	UFUNCTION(BlueprintCallable)
+		void SetRifleMode();
+
+	UFUNCTION(BlueprintCallable)
+		void SetSniperMode();
+
+private:
+	UFUNCTION()
+		void SetMode(EActionType InNewType);		// 무기를 들고 있을 때 같은 무기를 호출하면 Unarmed로 회귀시켜주는 함수
+
+	UFUNCTION()
+		void ChangeType(EActionType InNewType);
+
+public:		// Get Assets
+	UFUNCTION()
+		void DoAction();
+
+	UFUNCTION()
+		void DoAim(bool InPressed);
+
+
+	UFUNCTION()
+		void OffAllCollisions();		// 죽었을 때 무기 충돌체 Off
+
+	UFUNCTION()
+		void End_Dead();
+
+public:
+	UPROPERTY(BlueprintAssignable)
+		FActionTypeChangedSignature OnActionTypeChanged;
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+		class UCActionData* DataAssets[(int32)EActionType::Max];
+
+private:
+	EActionType Type;
+	class UCActionData_Spawned* Datas[(int32)EActionType::Max];
 };
