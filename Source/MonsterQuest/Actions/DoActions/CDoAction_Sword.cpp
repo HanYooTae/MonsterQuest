a18c623f052aa2cd38ab05b685aac6811901c838
HanYooTae <han931120@gmail.com>
2024-04-25 23:30:29 +0900

    Initialze Sword Actions

---
+++ Source/MonsterQuest/Actions/DoActions/CDoAction_Sword.cpp
@@ -0,0 +1,2 @@
+#include "Actions/DoActions/CDoAction_Sword.h"
+
---
+++ Source/MonsterQuest/Actions/DoActions/CDoAction_Sword.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actions/DoActions/CDoAction.h"
+#include "CDoAction_Sword.generated.h"
+
+UCLASS()
+class MONSTERQUEST_API ACDoAction_Sword : public ACDoAction
+{
+	GENERATED_BODY()
+	
+};
