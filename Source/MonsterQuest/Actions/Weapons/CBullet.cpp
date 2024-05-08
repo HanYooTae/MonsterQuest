d717cad8784407a9727d8362792d41497c4abfbe
HanYooTae <han931120@gmail.com>
2024-05-08 23:53:13 +0900

    Make Bullet

--- Source/MonsterQuest/Actions/Weapons/CBullet.cpp
+++ Source/MonsterQuest/Actions/Weapons/CBullet.cpp
@@ -1,12 +1,55 @@
 #include "Actions/Weapons/CBullet.h"
 
+#include "Components/StaticMeshComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Materials/MaterialInstanceConstant.h"
+
+#include "Global.h"
+
 ACBullet::ACBullet()
 {
-	
+	CHelpers::CreateSceneComponent<UCapsuleComponent>(this, &Capsule, "Capsule");
+	CHelpers::CreateSceneComponent<UStaticMeshComponent>(this, &Mesh, "Mesh", Capsule);
+	CHelpers::CreateActorComponent<UProjectileMovementComponent>(this, &Projectile, "Projectile");
+
+	Capsule->SetRelativeRotation(FRotator(90, 0, 0));
+	Capsule->SetCapsuleHalfHeight(50);
+	Capsule->SetCapsuleRadius(2);
+	Capsule->SetCollisionProfileName("BlockAllDynamic");
+
+	UStaticMesh* mesh;
+	CHelpers::GetAsset<UStaticMesh>(&mesh, "StaticMesh'/Game/Meshes/Sphere.Sphere'");
+	Mesh->SetStaticMesh(mesh);
+	Mesh->SetRelativeScale3D(FVector(1, 0.025f, 0.025f));
+	Mesh->SetRelativeRotation(FRotator(90, 0, 0));
+
+	UMaterialInstanceConstant* material;
+	CHelpers::GetAsset<UMaterialInstanceConstant>(&material, "MaterialInstanceConstant'/Game/Materials/M_Bullet_Inst.M_Bullet_Inst'");
+	Mesh->SetMaterial(0, material);
+
+
+	Projectile->InitialSpeed = 2e+4f; //2 * 10 ^ 4
+	Projectile->MaxSpeed = 2e+4f; //2 * 10 ^ 4
+	Projectile->ProjectileGravityScale = 0;
 }
 
 void ACBullet::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	Capsule->OnComponentBeginOverlap.AddDynamic(this, &ACBullet::OnComponentBeginOverlap);
+}
+
+void ACBullet::Shoot(const FVector& InDirection)
+{
+	Projectile->Velocity = InDirection * Projectile->InitialSpeed;
+}
+
+void ACBullet::OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OnBeginOverlap.IsBound())
+		OnBeginOverlap.Broadcast(SweepResult);
+
+	Destroy();
 }
--- Source/MonsterQuest/Actions/Weapons/CBullet.h
+++ Source/MonsterQuest/Actions/Weapons/CBullet.h
@@ -4,6 +4,8 @@
 #include "GameFramework/Actor.h"
 #include "CBullet.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FBulletBeginOverlapSignature, FHitResult, InHitResult);
+
 UCLASS()
 class MONSTERQUEST_API ACBullet : public AActor
 {
@@ -11,6 +13,14 @@ class MONSTERQUEST_API ACBullet : public AActor
 	
 public:	
 	ACBullet();
+
+public:
+	void Shoot(const FVector& InDirection);
+
+private:
+	UFUNCTION()
+		void OnComponentBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
 
 protected:
 	virtual void BeginPlay() override;
@@ -25,4 +35,7 @@ private:
 	UPROPERTY(VisibleDefaultsOnly)
 		class UProjectileMovementComponent* Projectile;
 
+public:
+	UPROPERTY(BlueprintAssignable)
+		FBulletBeginOverlapSignature OnBeginOverlap;
 };
