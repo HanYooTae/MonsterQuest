5e312eeac064208be67633778d8f9eb22a075245
HanYooTae <han931120@gmail.com>
2024-05-16 19:53:09 +0900

    Initialze Firing Settings

--- Source/MonsterQuest/Actions/Weapons/CWeapon_Guns.cpp
+++ Source/MonsterQuest/Actions/Weapons/CWeapon_Guns.cpp
@@ -1,2 +1,35 @@
 #include "Actions/Weapons/CWeapon_Guns.h"
 
+#include "CPlayer/CPlayer.h"
+
+ACWeapon_Guns::ACWeapon_Guns()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ACWeapon_Guns::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACWeapon_Guns::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+bool ACWeapon_Guns::CanFire()
+{
+	return false;
+}
+
+void ACWeapon_Guns::Begin_Fire()
+{
+}
+
+void ACWeapon_Guns::End_Fire()
+{
+}
+
+void ACWeapon_Guns::OnFiring()
+{
+}
--- Source/MonsterQuest/Actions/Weapons/CWeapon_Guns.h
+++ Source/MonsterQuest/Actions/Weapons/CWeapon_Guns.h
@@ -9,4 +9,37 @@ class MONSTERQUEST_API ACWeapon_Guns : public ACWeapon
 {
 	GENERATED_BODY()
 	
+public:
+	ACWeapon_Guns();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+
+public:
+	FORCEINLINE bool IsFiring() { return bFiring; }
+	FORCEINLINE bool IsAutoFire() { return bAutoFire; }
+
+	bool CanFire();
+	void Begin_Fire();
+	void End_Fire();
+
+	UFUNCTION()
+		void OnFiring();
+
+protected:
+	class UCUserWidget_CrossHair* CrossHair;
+
+	UPROPERTY(EditDefaultsOnly, Category = "HUD")
+		TSubclassOf<class UCUserWidget_CrossHair> CrossHairClass;
+
+private:
+	bool bFiring;
+	bool bAutoFire = true;
+
+private:
+	FTimerHandle AutoFireHandle;
 };
