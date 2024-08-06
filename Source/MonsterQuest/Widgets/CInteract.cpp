21bf9156b4893acc35b1ddaab8de0d015ec23457
HanYooTae <han931120@gmail.com>
2024-08-06 17:50:40 +0900

    Make Item Interact

--- Source/MonsterQuest/Widgets/CInteract.cpp
+++ Source/MonsterQuest/Widgets/CInteract.cpp
@@ -1,2 +1,17 @@
 #include "CInteract.h"
 
+#include "Components/TextBlock.h"
+
+#include "Global.h"
+
+bool UCInteract::Initialize() {
+	bool bCheck = Super::Initialize();
+	CheckFalseResult(bCheck, false);
+
+	return true;
+}
+
+void UCInteract::SetInteractText(FString Description)
+{
+	Interact->SetText(FText::FromString(Description));
+}
--- Source/MonsterQuest/Widgets/CInteract.h
+++ Source/MonsterQuest/Widgets/CInteract.h
@@ -9,4 +9,13 @@ class MONSTERQUEST_API UCInteract : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	virtual bool Initialize() override;
+
+public:
+	void SetInteractText(FString Description);
+
+private:
+	UPROPERTY(meta = (BindWidget))
+		class UTextBlock* Interact;
 };
